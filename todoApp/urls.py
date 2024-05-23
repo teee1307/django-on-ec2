9a30cdbfe8b4e2963530de2092056b3bfa0c13ff
teee1307 <156055248+teee1307@users.noreply.github.com>
2024-05-23 15:24:51 +0530

    Update urls.py

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -18,10 +18,12 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import settings, views
+from .views import health_check
 
 urlpatterns = [
     path('todos/', include('todos.urls')),
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index),
+     path('health-check/', health_check, name='health-check')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns()
